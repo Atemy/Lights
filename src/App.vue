855136258560df0a44897503072336385f281b18
Sokol <silvana93@mail.ru>
2021-10-22 19:09:12 +0300

    trable timer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test v-bind:class = "[colors[0], {Light:redIsLight}]" msg="Привет Тема!"/>
+    <Test v-bind:class = "[colors[1], {Light:yellowIsLight}]" msg="Смотри чо могу!"/>
+    <Test v-bind:class = "[colors[2], {Light:greenIsLight}]" msg="Опана! 3 цвета!"/>
+    <button v-on:click="changeColor">Изменить цвет!</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Test
+  },
+
+  data () {
+    return {
+      colors: [
+        'red',
+        'yellow',
+        'green'
+      ],
+      redIsLight: true,
+      yellowIsLight: false,
+      greenIsLight: false,
+      countColors: 1,
+      countDown: 10
+    }
+  },
+  methods: {
+    countDownTimer: function () {
+      if (this.countDown > 0) {
+        setTimeout(() => {
+          this.countDown -= 1
+          this.countDownTimer()
+        }, 1000)
+        console.log('Таймер кончился')
+        this.yellowIsLight = !this.yellowIsLight
+      }
+    },
+    yellowLightOn: function () {
+      console.log('Желтый')
+      this.greenIsLight = false
+      this.redIsLight = false
+      this.yellowIsLight = true
+      this.countDownTimer()
+    },
+    changeColor: function () {
+      console.log(this.countColors)
+      switch (this.countColors) {
+        case 0:
+          console.log('Красный!')
+          this.yellowLightOn()
+          this.redIsLight = !this.redIsLight
+          this.countColors++
+          break
+        case 1:
+          console.log('Зеленый')
+          this.yellowLightOn()
+          this.countColors = 0
+          this.greenIsLight = !this.greenIsLight
+
+          break
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .red{
+    background-color: red;
+    opacity: 0.3;
+  }
+  .yellow{
+    background-color: yellow;
+    opacity: 0.3;
+  }
+  .green{
+    background-color: green;
+    opacity: 0.3;
+  }
+  .Light{
+    opacity: 1;
+  }
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="lightDiv"></div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+.lightDiv{
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  border: black 1px solid;
+  margin-bottom: 5px;
+}
+</style>
